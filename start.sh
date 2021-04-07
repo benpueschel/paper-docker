ca26f8567070a54b9614fbc2a09f22ddc329bccf
Ben Püschel <ben.pueschel2004@gmail.com>
2021-04-07 21:23:30 +0000

    Add JVM RAM option

--- start.sh
+++ start.sh
@@ -5,23 +5,24 @@ cd paper
 
 MC_VERSION=${1:-latest}
 PAPER_BUILD=${2:-latest}
+MC_RAM=${3:-1G}
 
 
 # Perform initial setup
 URL_PREFIX=https://papermc.io/api/v2/projects/paper
-if [ MC_VERSION = latest ]
+if [ ${MC_VERSION} = latest ]
   then
     # Get the latest MC version
     MC_VERSION=$(wget -qO - $URL_PREFIX | jq -r '.versions[-1]') # "-r" is needed because the output has quotes otherwise
 fi
 URL_PREFIX=${URL_PREFIX}/versions/${MC_VERSION}
-if [ PAPER_BUILD = latest ]
+if [ ${PAPER_BUILD} = latest ]
   then
     # Get the latest build
     PAPER_BUILD=$(wget -qO - $URL_PREFIX | jq '.builds[-1]')
 fi
 
-JAR_NAME=papermc-${MC_VERSION}-${PAPER_BUILD}.jar
+JAR_NAME=paper-${MC_VERSION}-${PAPER_BUILD}.jar
 
 if [ ! -e ${JAR_NAME} ]
   then
@@ -33,3 +34,6 @@ if [ ! -e ${JAR_NAME} ]
       sed -i 's/false/true/g' eula.txt
     fi
 fi
+
+# Start server
+exec java -server -Xms${MC_RAM} -Xmx${MC_RAM} ${JAVA_OPTS} -jar ${JAR_NAME} nogui
