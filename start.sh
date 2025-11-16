65814b6a86c35ad8556016cc8250775dc6390c05
Ben Püschel <ben.pueschel2004@gmail.com>
2025-11-16 02:32:23 +0100

    feat: use new API endpoint
    Paper retired the old enpoint, so use the new API

--- start.sh
+++ start.sh
@@ -11,18 +11,21 @@ PAPER_BUILD=${PAPER_BUILD:-latest}
 MIN_RAM=${MIN_RAM:-256M}
 MAX_RAM=${MAX_RAM:-1G}
 
+
 # Perform initial setup
-URL_PREFIX=https://papermc.io/api/v2/projects/paper
+URL_PREFIX=https://fill.papermc.io/v3/projects/paper
 if [ ${MC_VERSION} = latest ]
   then
     # Get the latest MC version
-    MC_VERSION=$(wget -qO - $URL_PREFIX | jq -r '.versions[-1]') # "-r" is needed because the output has quotes otherwise
+	MC_VERSION=$(wget -qO - $URL_PREFIX | jq -r '.versions[ (.versions | keys_unsorted | first) ] | first')
+	echo "Latest MC version detected: ${MC_VERSION}"
 fi
 URL_PREFIX=${URL_PREFIX}/versions/${MC_VERSION}
 if [ ${PAPER_BUILD} = latest ]
   then
     # Get the latest build
-    PAPER_BUILD=$(wget -qO - $URL_PREFIX | jq '.builds[-1]')
+    PAPER_BUILD=$(wget -qO - $URL_PREFIX/builds/latest | jq -r '.id')
+	echo "Latest Paper build detected: ${PAPER_BUILD}"
 fi
 
 JAR_NAME=paper-${MC_VERSION}-${PAPER_BUILD}.jar
@@ -30,7 +33,8 @@ JAR_NAME=paper-${MC_VERSION}-${PAPER_BUILD}.jar
 if [ ! -e ${JAR_NAME} ]
   then
     rm -f *.jar
-    wget ${URL_PREFIX}/builds/${PAPER_BUILD}/downloads/paper-${MC_VERSION}-${PAPER_BUILD}.jar -O ${JAR_NAME}
+	DOWNLOAD_URL=$(wget -qO - $URL_PREFIX/builds/${PAPER_BUILD} | jq -r '.downloads."server:default".url')
+    wget ${DOWNLOAD_URL} -O ${JAR_NAME}
 fi
 
 # Start server
